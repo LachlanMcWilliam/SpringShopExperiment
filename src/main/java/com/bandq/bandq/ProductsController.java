e2b8cfc29619d402217544ed80d207bb650b7490
Lachlan <mcwilliam01@gmail.com>
2018-12-18 19:01:52 +0000

    started get all products

--- src/main/java/com/bandq/bandq/ProductsController.java
+++ src/main/java/com/bandq/bandq/ProductsController.java
@@ -2,6 +2,8 @@ package com.bandq.bandq;
 
 import com.bandq.spring.DAO.ProductsDAO;
 import com.bandq.spring.DAO.ProductsImplementation;
+import com.bandq.spring.Entities.Product;
+import org.eclipse.jdt.internal.compiler.lookup.MostSpecificExceptionMethodBinding;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -11,29 +13,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import viewModels.ProductViewModel;
 import com.bandq.spring.Services.ProductService;
+import viewModels.ProductsView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 //@RequestMapping("/")
 public class ProductsController {
 
+    private ProductsDAO products;
     private ApplicationContext context;
     private ProductService productService;
 
     public ProductsController(){
         context = new ClassPathXmlApplicationContext("spring.xml");
         productService = (ProductService) context.getBean("productService");
+        products = new ProductsImplementation();
     }
 
-    @RequestMapping("/")
-    String index(Model model) {
-        model.addAttribute("msg", "Hello there, This message has been injected from the controller method");
-        return "products";
+    @RequestMapping("/products")
+    ModelAndView index() {
+        List<Product> listOfProducts = products.getAll();
+        products.flush();
+
+        ArrayList<ProductViewModel> listOfProductViews = new ArrayList<>();
+
+        for (Product productEntity : listOfProducts){
+            ProductViewModel product = new ProductViewModel(productEntity.getId(), productEntity.getName(), productEntity.getStock(), productEntity.getWeight(),
+                    productEntity.getPrice(), productEntity.getDescription());
+
+            listOfProductViews.add(product);
+        }
+
+
+        ModelAndView map = new ModelAndView("products");
+        map.addObject("products", listOfProductViews);
+
+        return map;
     }
 
-    @RequestMapping("/product/{id}")
+    @RequestMapping("/products/{id}")
     ModelAndView getProduct(@PathVariable("id") int id) {
-
-        ProductsDAO products = new ProductsImplementation();
 
         com.bandq.spring.Entities.Product productEntity = products.get(id);
         int int1 = productEntity.getPrice();
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,9 +1,8 @@
-<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
-
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <HTML>
  <head>
-    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.5/dist/popper.min.js"></script>
@@ -76,32 +75,6 @@
       <br>
       <br>
       <br>
-      <div class = "row">
-      <c:set var = "counter" value = "0" scope = "page"></c:set>
-      <c:forEach var = "productItem" items = "${products}">
-        <c:set var = "counter" value = "${counter + 1}" scope = "page"></c:set>
-        <div class="col-sm-4">
-            <div class="row">
-              <div class="col-sm-6">
-                <img class="img-thumbnail" src="images/rocks.jpg"></img>
-              </div>
-              <div class="col-sm-6">
-                <div class="row">
-                  ${productItem.description}
-                </div>
-                <div class="row">
-                  <a class="btn btn-primary btn-sm" href = "/products/${productItem.id}">Click here</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        <c:if test = "${counter == '3'}">
-          <c:set var = "counter" value = "0" scope = "page"></c:set>
-            </div>
-            <div class = "row">
-        </c:if>
-      </c:forEach>
-    </div>
       <div class="row">
         <div class="col-sm-4">
           <div class="row">
